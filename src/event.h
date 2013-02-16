6ebc3270646f491ed61b2dd3e2229a303776b87f
razic <razic@viralkitty.com>
2013-02-16 23:56:21 +0700

    add getLocation to event class

--- src/event.h
+++ src/event.h
@@ -16,6 +16,7 @@ class Event : public node::ObjectWrap {
     static v8::Handle<v8::Value> SetType(const v8::Arguments& args);
     static v8::Handle<v8::Value> SetIntegerValueField(const v8::Arguments& args);
     static v8::Handle<v8::Value> SetLocation(const v8::Arguments& args);
+    static v8::Handle<v8::Value> GetLocation(const v8::Arguments& args);
     static CGEventType TypeFromString(std::string str);
     static CGEventType ValueFieldFromString(std::string str);
 
